847d61f0499d7ee3a0d8eeb54b0e1ac7d4944968
Elwan Mayencourt <elwan.mayencourt@gmail.com>
2023-06-09 22:26:43 +0200

    💚 Load backend hostname from env

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.HOSTNAME ? `https://${import.meta.env.HOSTNAME}` : 'http://localhost:8000';
+export const BASE_URL = import.meta.env.VITE_HOSTNAME ? `https://${import.meta.env.VITE_HOSTNAME}` : 'http://localhost:8000';
 
 const getId = async () => {
 	return fetch(`${BASE_URL}/id`, {
